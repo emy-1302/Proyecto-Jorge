28a8fa13cfd563dccb5e1957d460b523cc6466b7
JORGE CORTES DOMINGUEZ <jorge@cbtis66.edu.mx>
2025-06-17 11:14:29 -0600

    Update db.js gatito gatito

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 console.log('Entorno cargado:', {
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_PASSWORD: process.env.DB_PASSWORD + '#',
   DB_NAME: process.env.DB_NAME
 });
 const mysql = require('mysql2/promise');
